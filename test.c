a6b58e4215771b1693b79cc7cd191fe3fe5bbc81
SamNavdeep <samknavdeep@gmail.com>
2026-07-09 16:48:08 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,4 +1,3 @@
-
 #include <stdint.h>
 #include "ARMCM4.h"
 
@@ -8,9 +7,11 @@
 volatile uint32_t current_task __attribute__((used)) = 0;
 uint32_t x = 0;
 uint32_t y = 0;
+uint32_t z = 0; 
 
 void scheduler(void){
-    current_task = (current_task + 1) % 2;
+
+    current_task = (current_task + 1) % 3;
 }
 
 void delay(){
@@ -28,7 +29,14 @@ void counter1(uint32_t* a){
 void counter2(uint32_t* b){
     while(1){
         delay();
-        *b += 1;
+        *b += 2;
+    }
+}
+
+void counter3(uint32_t* a){
+    while(1){
+        delay();
+        *a += 3;
     }
 }
 
@@ -39,14 +47,20 @@ struct task_info{
 
 uint32_t task1[STACK_SIZE];
 uint32_t task2[STACK_SIZE];
-struct task_info tasks[2] __attribute__((used)) = {{task1, 1}, {task2, 2}};
+uint32_t task3[STACK_SIZE];
+
+
+struct task_info tasks[3] __attribute__((used)) = {
+    {task1, 1}, 
+    {task2, 2}, 
+    {task3, 3}
+};
 
 void SysTick_Handler(void){
     static volatile uint32_t hardware_ticks = 0;
     hardware_ticks += 1;
     
     SCB->ICSR |= SCB_ICSR_PENDSVSET_Msk;
-	
 }
 
 __attribute__((naked)) void PendSV_Handler(void) {
@@ -55,54 +69,52 @@ __attribute__((naked)) void PendSV_Handler(void) {
         
         "PUSH {R4-R11}\n"        // Push remaining 8 registers
         
+        // --- SAVE CURRENT TASK CONTEXT ---
         "LDR R0, =current_task\n"
-        "LDR R1, [R0]\n"         
-        "CBZ R1, skip_save_2\n"  
-        
-        "LDR R2, =tasks\n"       
-        "ADD R2, #8\n"           
-        "STR SP, [R2]\n"         // Store SP into tasks[1]
-        "B next_task\n"
-        
-        "skip_save_2:\n"
+        "LDR R1, [R0]\n"         // R1 = current_task index (0, 1, or 2)
         "LDR R2, =tasks\n"
-        "STR SP, [R2]\n"         // Store SP into tasks[0]
+        "LSL R1, R1, #3\n"       // R1 = current_task * 8 (sizeof struct is 8 bytes)
+        "ADD R2, R2, R1\n"       // R2 = &tasks[current_task]
+        "STR SP, [R2]\n"         // Save current SP into tasks[current_task].sp
         
-        "next_task:\n"
-        "PUSH {LR}\n"            // Save the magic EXC_RETURN number
+        // --- CALL SCHEDULER ---
+        "PUSH {LR}\n"            // Save the EXC_RETURN number
         "BL scheduler\n"         // Call our C scheduler
-        "POP {LR}\n"             // Restore the magic number
+        "POP {LR}\n"             // Restore the number
         
+        // --- LOAD NEXT TASK CONTEXT ---
         "LDR R0, =current_task\n"
-        "LDR R1, [R0]\n"         
+        "LDR R1, [R0]\n"         // R1 = new current_task index
         "LDR R2, =tasks\n"
-        "CBZ R1, load_task1\n"
-        "ADD R2, #8\n"           
-        
-        "load_task1:\n"
+        "LSL R1, R1, #3\n"       // R1 = new current_task * 8
+        "ADD R2, R2, R1\n"       // R2 = &tasks[new current_task]
         "LDR SP, [R2]\n"         // Load new task's SP 
         
         "POP {R4-R11}\n"         // Pop the 8 manual registers
         
         "CPSIE I\n"              // Re-enable interrupts
-        "BX LR\n"                // Hardware auto-pops and resumes task!
+        "BX LR\n"                // Hardware auto-pops and resumes task
     );
 }
 
 void os_init(void){
-    // --- Task 1 Setup ---
+
     tasks[0].sp = &task1[STACK_SIZE - 16]; 
     task1[STACK_SIZE - 1] = 0x01000000;    
     task1[STACK_SIZE - 2] = ((uint32_t)counter1); 
-    
     task1[STACK_SIZE - 8] = (uint32_t)&x;  
 
-    // --- Task 2 Setup ---
+
     tasks[1].sp = &task2[STACK_SIZE - 16]; 
     task2[STACK_SIZE - 1] = 0x01000000;
     task2[STACK_SIZE - 2] = ((uint32_t)counter2); 
-    
     task2[STACK_SIZE - 8] = (uint32_t)&y;  
+
+
+    tasks[2].sp = &task3[STACK_SIZE - 16]; 
+    task3[STACK_SIZE - 1] = 0x01000000;
+    task3[STACK_SIZE - 2] = ((uint32_t)counter3); 
+    task3[STACK_SIZE - 8] = (uint32_t)&z;  
 }
 
 // The OS Bootloader
